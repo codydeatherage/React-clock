a0e43b26d5286d5ff42e7242bd4b11b6e042707b
codydeatherage <cdeatherage14@gmail.com>
2021-02-26 21:41:19 -0800

    first commit

---
+++ react-clock/src/App.js
@@ -0,0 +1,24 @@
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  return (
+    <div className="container">
+      <div className="box">
+        <div className="clock-stick">
+          <div>0</div>
+          <div>1</div>
+          <div>2</div>
+        </div>
+        <div className="clock-stick"></div>
+        <div className="clock-stick"></div>
+        <div className="clock-stick"></div>
+        <div className="clock-stick"></div>
+        <div className="clock-stick"></div>
+
+      </div>
+    </div>
+  );
+}
+
+export default App;
